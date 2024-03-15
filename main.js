2071b8cafa18fe8754415f6c053ddf9cf5bf54a0
JeiberDev <jeibergarciayt@gmail.com>
2024-03-15 18:32:12 -0500

    Crea funciones para realizar operaciones de suma, resta, multiplicación y división.

---
+++ main.js
@@ -0,0 +1,19 @@
+function sumar(a, b) {
+    return a + b;
+  }
+  
+function restar(a, b) {
+    return a - b;
+  }
+  
+function multiplicar(a, b) {
+    return a * b;
+  }
+  
+function dividir(a, b) {
+    if (b === 0) {
+      throw new Error("No se puede dividir por cero");
+    }
+    return a / b;
+  }
+  
